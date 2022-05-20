e0cb73ea191659971077cc57459f275536bfbd96
theresa-de-ocampo <ma_theresa7@yahoo.com>
2022-05-20 17:05:49 +0800

    Styled System - Layout, W, & H

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import { ChakraProvider, Box, Heading, Text, Flex, theme } from "@chakra-ui/react"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+    return (
+        <ChakraProvider theme={theme}>
+           <Box w="200px" h="200px" bgGradient="linear(green, pink)" color="white" mb="8">
+               <Text fontSize="4xl" fontFamily="mono">Theresa</Text>
+           </Box>
+           <Heading color="red.600">Hello World</Heading>
+           <Box boxSize="md" bg="red"></Box>
+        </ChakraProvider>
+    );
 }
-
-export default App;
