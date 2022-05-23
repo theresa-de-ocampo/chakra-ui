9e35f0e2b7aea27c1e3d8bfeff58f06ea3606f17
theresa-de-ocampo <ma_theresa7@yahoo.com>
2022-05-23 14:29:12 +0800

    Components > Form Control

--- src/App.js
+++ src/App.js
@@ -7,7 +7,19 @@ import {
     ButtonGroup,
     IconButton,
     Checkbox,
-    CheckboxGroup
+    CheckboxGroup,
+    Editable,
+    EditableInput,
+    EditableTextarea,
+    EditablePreview,
+    FormControl,
+    FormLabel,
+    FormHelperText,
+    FormErrorMessage,
+    Input,
+    RadioGroup,
+    Radio,
+    Select
 } from "@chakra-ui/react"
 import { MdBuild, MdCall} from "react-icons/md"
 import { BiTachometer } from "react-icons/bi"
@@ -84,6 +96,42 @@ export default function App() {
                     <Checkbox value="php">PHP</Checkbox>
                 </CheckboxGroup>
             </Box>
+
+            <Editable defaultValue="Theresa De Ocampo" my={8}>
+                <EditableInput />
+                <EditablePreview />
+            </Editable>
+
+            <Editable defaultValue="I'm a Web Developer Intern" my={8}>
+                <EditableTextarea />
+                <EditablePreview />
+            </Editable>
+
+            <FormControl isRequired mb={8}>
+                <FormLabel htmlFor="email">Email</FormLabel>
+                <Input id="email" type="email" />
+                <FormHelperText>We'll never share your email.</FormHelperText>
+            </FormControl>
+
+            <FormControl as="fieldset" mb={8}>
+                <FormLabel as="legend">Favorite Stardew Valley Characters</FormLabel>
+                <RadioGroup defaultValue="Shane">
+                    <Radio value="sebastian">Sebastian</Radio>
+                    <Radio value="shane">Shane</Radio>
+                    <Radio value="harvey">Harvey</Radio>
+                    <Radio value="alex">Alex</Radio>
+                </RadioGroup>
+            </FormControl>
+
+            <FormControl mb={8}>
+                <FormLabel htmlFor="country">Country</FormLabel>
+                <Select id="country" placeholder="Select Country">
+                    <option>Philippines</option>
+                    <option>Singapore</option>
+                    <option>New Zealand</option>
+                    <option>Canada</option>
+                </Select>
+            </FormControl>
         </ChakraProvider>
     );
 }
